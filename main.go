21cbe8c008ca385a6e468b185638aa077d4a554a
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-07-14 08:24:19 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
